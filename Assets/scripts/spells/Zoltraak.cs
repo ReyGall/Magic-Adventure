9767bdf90305070aca38a5553c07c596ebe2a68c
Rey <romangaltsev.rey@gmail.com>
2026-09-26 16:41:31 +0300

    Add input actions and input manager for spell casting mechanics
    - Implement InputActions class for managing player input.
    - Create InputManager to handle ability slots and input actions.
    - Update Shield and Zoltraak classes to utilize new input system for casting spells.
    - Refactor spell_base to include virtual methods for casting, holding, and stopping spells.
    - Updated Roadmap

--- Assets/scripts/spells/Zoltraak.cs
+++ Assets/scripts/spells/Zoltraak.cs
@@ -2,7 +2,6 @@ using MyGame.Damage_interface;
 using MyGame.manaControl;
 using MyGame.SpellsCore;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 namespace MyGame.SpellZoltraak
 {
@@ -33,56 +32,48 @@ namespace MyGame.SpellZoltraak
             _damage = 50f;
         }
 
-        private void Update()
+        public override void Cast()
         {
-            if (Keyboard.current.eKey.wasPressedThisFrame)
+            if (SpellCheck())
             {
-                if (SpellCheck())
-                {
-                    Vector3 localOffset = new Vector3(0f, 0f, 0f);
-                    Vector3 spawnPosition =
-                        _spawnPoint.position + _spawnPoint.TransformDirection(localOffset);
-                    Quaternion spawnRotation = _spawnPoint.rotation * Quaternion.Euler(0f, 0f, 0f);
-                    GameObject vfxInstance = Instantiate(_vfxPrefab, spawnPosition, spawnRotation);
-                    Destroy(vfxInstance, 3f);
-                    Vector3 localOffset2 = new Vector3(0f, 1f, 1.5f);
-                    Vector3 spawnPosition2 =
-                        _spawnPoint2.position + _spawnPoint2.TransformDirection(localOffset2);
-                    Quaternion spawnRotation2 =
-                        _spawnPoint2.rotation * Quaternion.Euler(0f, 270f, 0f);
-                    GameObject vfxInstance2 = Instantiate(
-                        _vfxPrefab2,
-                        spawnPosition2,
-                        spawnRotation2
-                    );
-                    RaycastHit hit;
-                    Debug.DrawRay(
+                Vector3 localOffset = new Vector3(0f, 0f, 0f);
+                Vector3 spawnPosition =
+                    _spawnPoint.position + _spawnPoint.TransformDirection(localOffset);
+                Quaternion spawnRotation = _spawnPoint.rotation * Quaternion.Euler(0f, 0f, 0f);
+                GameObject vfxInstance = Instantiate(_vfxPrefab, spawnPosition, spawnRotation);
+                Destroy(vfxInstance, 3f);
+                Vector3 localOffset2 = new Vector3(0f, 1f, 1.5f);
+                Vector3 spawnPosition2 =
+                    _spawnPoint2.position + _spawnPoint2.TransformDirection(localOffset2);
+                Quaternion spawnRotation2 = _spawnPoint2.rotation * Quaternion.Euler(0f, 270f, 0f);
+                GameObject vfxInstance2 = Instantiate(_vfxPrefab2, spawnPosition2, spawnRotation2);
+                RaycastHit hit;
+                Debug.DrawRay(
+                    _spawnPoint2.position,
+                    -_spawnPoint2.right * _defaultDistance,
+                    Color.red,
+                    1f
+                );
+                if (
+                    Physics.SphereCast(
                         _spawnPoint2.position,
-                        -_spawnPoint2.right * _defaultDistance,
-                        Color.red,
-                        1f
-                    );
-                    if (
-                        Physics.SphereCast(
-                            _spawnPoint2.position,
-                            _radius,
-                            _spawnPoint2.forward,
-                            out hit,
-                            _defaultDistance,
-                            _enemyLayer
-                        )
+                        _radius,
+                        _spawnPoint2.forward,
+                        out hit,
+                        _defaultDistance,
+                        _enemyLayer
                     )
+                )
+                {
+                    if (hit.collider != null)
                     {
-                        if (hit.collider != null)
+                        if (hit.collider.TryGetComponent<IDamageable>(out var damageable))
                         {
-                            if (hit.collider.TryGetComponent<IDamageable>(out var damageable))
-                            {
-                                damageable.TakeDamage(_damage);
-                            }
+                            damageable.TakeDamage(_damage);
                         }
                     }
-                    Destroy(vfxInstance2, 3f);
                 }
+                Destroy(vfxInstance2, 3f);
             }
         }
     }
--- README.md
+++ README.md
@@ -59,7 +59,7 @@ The project is currently in its early prototype stage.
 [✓] First spell
 [✓] Modular spell system
 [✓] Basic enemy
-[ ] Modular input system
+[✓] Modular input system
 [ ] GUI
 [ ] Combat loop
 [ ] Prototype arena
